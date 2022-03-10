c6caa5d2faf60b1bdcd5e1e24f6d0243d5138d8a
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-03-10 11:06:20 +0100

    working on react-native

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, View, SafeAreaView, StatusBar } from 'react-native';
+import { Searchbar } from 'react-native-paper';
+import RestaurantInfo from '../components/restaurant-info-component';
+
+const RestaurantScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar placeholder='Search' />
+      </View>
+      <View style={styles.list}>
+        <RestaurantInfo />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, marginTop: StatusBar.currentHeight },
+  search: { padding: 16 },
+  list: { flex: 1, padding: 16 },
+});
+
+export default RestaurantScreen;
