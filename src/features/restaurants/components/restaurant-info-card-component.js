13ed40c83083e27af776504efaa6e7587728ddd3
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-03-10 16:15:48 +0100

    working on react-native

--- src/features/restaurants/components/restaurant-info-card-component.js
+++ src/features/restaurants/components/restaurant-info-card-component.js
@@ -1,50 +1,20 @@
 import React from 'react';
-import { Text, Image, View } from 'react-native';
-import styled from 'styled-components/native';
-import { Card } from 'react-native-paper';
 import { SvgXml } from 'react-native-svg';
+import { Spacer } from '../../../components/spacer/spacer.component';
+import { Text } from '../../../components/typography/text.component';
 import star from '../../../../assets/star';
 import open from '../../../../assets/open';
-import { Spacer } from '../../../components/spacer/spacer.component';
 
-const RestaurantCard = styled(Card)`
-  background: ${(props) => props.theme.colors.bg.primary};
-`;
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]};
-  background: #fff;
-`;
-
-const Address = styled.Text`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
-const Title = styled.Text`
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
-
-const Info = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Section = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const SectionEnd = styled.View`
-  flex-direction: row;
-`;
+import {
+  Icon,
+  RestaurantCard,
+  RestaurantCardCover,
+  Address,
+  Info,
+  Section,
+  Rating,
+  SectionEnd,
+} from './restaurant-info-card.styles';
 
 const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
@@ -63,7 +33,7 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
     <RestaurantCard elevation={5}>
       <RestaurantCardCover source={{ uri: photos }} key={name} />
       <Info>
-        <Title>{name}</Title>
+        <Text variant='label'>{name}</Text>
         <Section>
           <Rating>
             {ratingArray.map((_, index) => (
@@ -72,13 +42,13 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
           </Rating>
           <SectionEnd>
             {isClosedTemporarily && (
-              <Text variant='label'>CLOSED TEMPORARILY</Text>
+              <Text variant='error'>CLOSED TEMPORARILY</Text>
             )}
             <Spacer position='left' size='large'>
               {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
             </Spacer>
             <Spacer position='left' size='large'>
-              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+              <Icon source={{ uri: icon }} />
             </Spacer>
           </SectionEnd>
         </Section>
