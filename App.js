1fabc6a8f95a8a204cd8c24b631d86162be9089a
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-03-12 10:12:02 +0100

    working on react-native

--- App.js
+++ App.js
@@ -19,9 +19,23 @@ import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context';
 import { LocationContextProvider } from './src/services/location/location.context';
 import { FavouriteContextProvider } from './src/services/favourites/favourites.context';
+import { AuthenticationContextProvider } from './src/services/authentication/authentication.context';
 
 // Screen Navigator
 import { Navigation } from './src/infrastructure/navigation';
+
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyChncgb60ofHWpRt51bPPH_1FMLtkxBcSM',
+  authDomain: 'reactnative-mealstogo.firebaseapp.com',
+  projectId: 'reactnative-mealstogo',
+  storageBucket: 'reactnative-mealstogo.appspot.com',
+  messagingSenderId: '225413781277',
+  appId: '1:225413781277:web:1351e230ef7d79458373f4',
+};
+
+initializeApp(firebaseConfig);
 
 export default function App() {
   const [oswalLoaded] = useOswald({
@@ -39,13 +53,15 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouriteContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouriteContextProvider>
+        <AuthenticationContextProvider>
+          <FavouriteContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavouriteContextProvider>
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style='auto' />
     </>
--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,9 +1,30 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const FavouriteContext = createContext();
 
 export const FavouriteContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
+
+  const saveFavourites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem('@favourites', jsonValue);
+    } catch (err) {
+      console.log('error storing', err);
+    }
+  };
+
+  const loadFavourites = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@favourites');
+      if (value !== null) {
+        setFavourites(JSON.parse(value));
+      }
+    } catch (err) {
+      console.log('error loading', err);
+    }
+  };
 
   const add = (restaurant) => {
     setFavourites([...favourites, restaurant]);
@@ -16,6 +37,15 @@ export const FavouriteContextProvider = ({ children }) => {
 
     setFavourites(newFavourites);
   };
+
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
+
   return (
     <FavouriteContext.Provider
       value={{
